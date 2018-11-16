5c5727e01cacf1835860aea370ade01632499ca1
disconaked <nolan.klinke@gmail.com>
2018-11-15 21:31:37 -0500

    basic form done

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,22 +1,29 @@
 import React from "react";
-// import "./ContactForm.css";
+import "./ContactForm.css";
 
 const ContactForm = () => (
   <div className="container">
+    <div className="formTitle">
+      <h1>Contact Me</h1>
+      <p>
+        I'd love to work with you! Fill out the form below and I will be in
+        touch shortly.
+      </p>
+    </div>
     <form action="https://formspree.io/nolan.klinke@gmail.com" method="POST">
       <div className="row">
-        <div className="col-25">
+        <div className="col-20">
           <label for="name">Name</label>
         </div>
-        <div className="col-75">
+        <div className="col-80">
           <input id="name" type="text" name="name" placeholder="Your Name" />
         </div>
       </div>
       <div className="row">
-        <div className="col-25">
+        <div className="col-20">
           <label for="email">Email</label>
         </div>
-        <div className="col-75">
+        <div className="col-80">
           <input
             id="email"
             type="email"
@@ -26,21 +33,24 @@ const ContactForm = () => (
         </div>
       </div>
       <div className="row">
-        <div className="col-25">
+        <div className="col-20">
           <label for="message">Message</label>
         </div>
-        <div className="col-75">
+        <div className="col-80">
           <input type="text" name="_gotcha" style={{ display: "none" }} />
-          <input
+          <textarea
             style={{ height: "200px" }}
             id="message"
             type="text"
             name="message"
-            placeholder="Write something..."
+            placeholder="Write
+            something..."
           />
         </div>
       </div>
-      <input type="submit" value="Send" />
+      <div id="btn">
+        <input type="submit" value="Send" />
+      </div>
     </form>
   </div>
 );
