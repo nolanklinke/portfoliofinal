94790bb304603c2f14a0b26b45447ac2471c5253
disconaked <nolan.klinke@gmail.com>
2018-11-18 15:05:30 -0500

    mobile additions and letter spacing

--- src/components/IconBar/IconBar.js
+++ src/components/IconBar/IconBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./IconBar.css";
 
 const IconBar = () => (
-  <div>
+  <div className="link-icons">
     <a
       href="https://www.linkedin.com/in/NolanKlinke"
       className="fab fa-linkedin"
