314f42bcb0634fc36c78c0988f078541dd105188
disconaked <nolan.klinke@gmail.com>
2019-02-17 19:03:54 -0500

    update prof

--- src/components/Aboutme/Aboutme.js
+++ src/components/Aboutme/Aboutme.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Aboutme = () => (
   <div>
-    <h1 id="abouth1">hi.</h1>
+    <h1 id="abouth1">hello!</h1>
     <p id="aboutP">
-      My name is Nolan Klinke and I am a Full Stack Web Developer. I have
-      experience in many different technologies, but consider myself proficient
-      in the MERN stack. I enjoy building new applications and creating dynamic
-      experiences through DOM manipulation and custom styling. I am a die-hard
-      problem solver at heart and thoroughly enjoy the satisfaction of figuring
-      out a complicated task or challenge. The things I enjoy most are spending
-      time with my wife, Brianna, our pup, Wiggles, video games, and indulging
-      in a donut or two.
+      My name is Nolan Klinke and I am a Full Stack Web Developer. I am
+      comfortable working with many different technologies and really enjoy
+      building new applications and creating dynamic experiences through DOM
+      manipulation, database management, and custom styling. I am a die-hard
+      problem solver at heart and find it thrilling being able to figure out a
+      complicated task or challenge. The things I enjoy most are spending time
+      with my wife, Brianna, our pup, Wiggles, video games, and indulging in a
+      donut or two.
     </p>
   </div>
 );
